281de5c1f436523e214a265e99a2b37b2020df43
MohammadMahdiketabchi <100718212+MohammadMahdiketabchi@users.noreply.github.com>
2024-04-27 01:54:18 +0330

    cleaner Lex_analyzer.py

--- Lex_analyzer.py
+++ Lex_analyzer.py
@@ -48,56 +48,7 @@ Type :dict = {
     '&&': 'T_LOp_AND',
     '||': 'T_LOp_OR',
 }
-#کلمات کلیدی1.1
-KEYWORDS = {
-    "bool": "T_Bool",
-    "break": "T_Break",
-    "char": "T_Char",
-    "continue": "T_Continue",
-    "else": "T_Else",
-    "false": "T_False",
-    "for": "T_For",
-    "if": "T_If",
-    "int": "T_Int",
-    "print": "T_Print",
-    "return": "T_Return",
-    "true": "T_True",
-}
 
-#علامت های نشانه گذاری 3.1
-Symbol = {
-    '(': 'T_LP',
-    ')': 'T_RP',
-    '{': 'T_LC',
-    '}': 'T_RC',
-    '[': 'T_LB',
-    ']': 'T_RB',
-    ';': 'T_Semicolon',
-    ',': 'T_Comma',
-}
-
-#عملگرهای تک حرفی
-SINGLE_CHAR_OPERATORS = {
-    '+': "T_AOp_PL",
-    '-': "T_AOp_MN",
-    '*': "T_AOp_ML",
-    '/': "T_AOp_DV",
-    '%': "T_AOp_RM",
-    '=': "T_Assign",
-    '<': 'T_ROp_L',
-    '>': 'T_ROp_G',
-    '!': 'T_LOp_NOT',
-}
-#عملگرهای دو حرفی
-DOUBLE_CHAR_OPERATORS = {
-    '<=': 'T_ROp_LE',
-    '>=': 'T_ROp_LE',
-    '!=': 'T_ROp_NE',
-    '==': 'T_ROp_E',
-    '&&': 'T_LOp_AND',
-    '||': 'T_LOp_OR',
-
-}
 symboleTable = SymbolTable.SymbolTable()
 def is_digit(character):
     return '0' <= character <= '9'
